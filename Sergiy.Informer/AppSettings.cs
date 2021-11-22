5b73bf6ef388cd9526fc3215425aaeff58b5363f
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2021-11-22 22:38:17 +0200

    init

---
+++ Sergiy.Informer/AppSettings.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Sergiy.Informer {
+
+  public class AppSettings {
+
+    private static readonly Lazy<AppSettings> InstanceField = new Lazy<AppSettings>(Load);
+    internal static AppSettings Instance => InstanceField.Value;
+
+    public int Left;
+    public int Top;
+    public int Height;
+    public int Width;
+    public double Opacity;
+    public bool StickToEdges;
+    public bool Weather;
+    
+    public int ChartViewMode;
+    public bool ChartLines;
+    
+    public int CurrencyIndex;
+    public readonly int RefreshInterval;
+
+    public AppSettings() {
+      Left = Screen.PrimaryScreen.WorkingArea.Left;// Screen.PrimaryScreen.WorkingArea.Right - Width;
+      Top = Screen.PrimaryScreen.WorkingArea.Top;
+      Height = 245;
+      Width = 175;
+      Opacity = 1;
+      StickToEdges = true;
+      Weather = false;
+    
+      ChartViewMode = 0;
+      ChartLines = false;
+
+      CurrencyIndex = 0;
+      RefreshInterval = 30;
+    }
+
+    #region Load/Save
+
+    public static string GetAppPath() {
+      return Assembly.GetExecutingAssembly().Location;
+    }
+
+    private static string GetSettingsFilePath() {
+      return Path.ChangeExtension(GetAppPath(), ".json");
+    }
+
+    internal void Save() {
+      var fileName = GetSettingsFilePath();
+      GC.Collect(GC.MaxGeneration);
+      File.WriteAllText(fileName, this.ToJson());
+    }
+
+    private static AppSettings Load() {
+      var fileName = GetSettingsFilePath();
+      try {
+        if (File.Exists(fileName)) {
+          var fileData = File.ReadAllText(fileName);
+          var result = fileData.FromJson<AppSettings>();
+          if (result != null)
+            return result;
+        }
+      }
+      catch {
+        // ignored
+      }
+
+      return new AppSettings();
+    }
+
+    #endregion Load/Save
+  }
+}
---
+++ Sergiy.Informer/Common.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sergiy.Informer {
+
+  internal class Common {
+
+    internal const int WmSyscommand = 0x112;
+    internal const int MfString = 0x0;
+    internal const int MfSeparator = 0x800;
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    internal static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    internal static extern bool AppendMenu(IntPtr hMenu, int uFlags, int uIdNewItem, string lpNewItem);
+
+    internal static string GetDeveloperText() {
+      return Encoding.ASCII.GetString(new byte[]
+      {
+                0x57, 0x72, 0x69, 0x74, 0x74, 0x65, 0x6E, 0x20, 0x62, 0x79, 0x20, 0x53, 0x65, 0x72, 0x67, 0x65,
+                0x79, 0x20, 0x45, 0x67, 0x6F, 0x73, 0x68, 0x69, 0x6E, 0x20, 0x28, 0x65, 0x67, 0x6F, 0x73, 0x68,
+                0x69, 0x6E, 0x2E, 0x73, 0x65, 0x72, 0x67, 0x65, 0x79, 0x40, 0x67, 0x6D, 0x61, 0x69, 0x6C, 0x2E,
+                0x63, 0x6F, 0x6D, 0x29
+      });
+    }
+
+    internal static bool DoSnap(int pos, int edge) {
+      const int snapDist = 50;
+      var delta = Math.Abs(pos - edge);
+      return delta <= snapDist;
+    }
+
+    internal static void UpdateBrowser(WebBrowser browser, string documentData) {
+      browser.DocumentText = "0";
+      if (browser.Document != null) {
+        browser.Document.OpenNew(true);
+        browser.Document.Write(documentData);
+      }
+      browser.Refresh();
+    }
+  }
+}
