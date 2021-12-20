a8dac2c7b82b0cef349e86061ec482e431b32c44
Sergey Egoshin <egoshin.sergey@gmail.com>
2021-12-20 22:26:25 +0200

    android: eur added on NbuPage

--- currency.common/Models/NbuRateItem.cs
+++ currency.common/Models/NbuRateItem.cs
@@ -10,19 +10,4 @@ namespace currency {
     
     public decimal Rate { get; set; }
   }
-
-  public class NbuRateModel : NbuRateItem {
-    
-    public string DateFormatted => Date.ToString("M");
-    public string RateFormatted => Rate.ToString("n3");
-    public string Color { get; set; }
-    public string Direction { get; set; }
-
-    public NbuRateModel(NbuRateItem value) {
-      this.Date = value.Date;
-      Direction = "";
-      Color = "White";
-      this.Rate = value.Rate;
-    }
-  }
 }
