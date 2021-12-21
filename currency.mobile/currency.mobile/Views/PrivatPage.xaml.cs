b87d113fed9be5c2304a93108eb0e4b4c060f600
Sergey Egoshin <egoshin.sergey@gmail.com>
2021-12-21 21:06:26 +0200

    Android: charts added to privat24 page

--- currency.mobile/currency.mobile/Views/PrivatPage.xaml.cs
+++ currency.mobile/currency.mobile/Views/PrivatPage.xaml.cs
@@ -2,6 +2,10 @@
 using System.Globalization;
 using System.Linq;
 using currency.mobile.Services;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.Xamarin.Forms;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,19 +20,8 @@ namespace currency.mobile.Views {
 
       InitializeComponent();
 
-      var grid = new Grid {
-        Padding = 5,
-        RowDefinitions = new RowDefinitionCollection(), //todo: in loop
-        ColumnDefinitions = new ColumnDefinitionCollection {
-          new ColumnDefinition {Width = new GridLength(3, GridUnitType.Star)},
-          new ColumnDefinition {Width = new GridLength(2, GridUnitType.Star)},
-          new ColumnDefinition {Width = new GridLength(2, GridUnitType.Star)},
-          new ColumnDefinition {Width = new GridLength(2, GridUnitType.Star)},
-          new ColumnDefinition {Width = new GridLength(2, GridUnitType.Star)},
-        },
-      };
-      
       var refreshView = new RefreshView();
+      var mainContent = new StackLayout { Orientation = StackOrientation.Vertical, Padding = new Thickness(0, 5, 0, 5) };
 
       async void ExecuteRefresh() {
 
@@ -46,20 +39,10 @@ namespace currency.mobile.Views {
           if (historyData.Data.History.Length <= 0) return;
 
           // Refresh data here
-          grid.Children.Clear();
-          grid.RowDefinitions.Clear();
-
-          grid.Children.Add(Utils.GetLabel("Date", 14, Color.LightCyan), 0, grid.RowDefinitions.Count);
-          grid.Children.Add(Utils.GetLabel("Buy", 14, Color.LightCyan), 1, grid.RowDefinitions.Count);
-          grid.Children.Add(Utils.GetLabel("B delta", 14, Color.LightCyan), 2, grid.RowDefinitions.Count);
-          grid.Children.Add(Utils.GetLabel("Sell", 14, Color.LightCyan), 3, grid.RowDefinitions.Count);
-          grid.Children.Add(Utils.GetLabel("S delta", 14, Color.LightCyan), 4, grid.RowDefinitions.Count);
-          grid.RowDefinitions.Add(new RowDefinition {Height = GridLength.Auto});
+          mainContent.Children.Clear();
 
           for (var currencyIndex = 0; currencyIndex < 2; currencyIndex++) {
             var currencyCode = Helper.GetCurrencyName(currencyIndex);
-            grid.Children.Add(Utils.GetLabel(currencyCode.ToUpper(), 16, Color.LightCyan), 2, grid.RowDefinitions.Count);
-            grid.RowDefinitions.Add(new RowDefinition {Height = GridLength.Auto});
 
             var data = historyData.Data.History.Where(i => i.CurrencyCode.Equals(currencyCode, StringComparison.OrdinalIgnoreCase))
               .OrderByDescending(x => {
@@ -68,6 +51,31 @@ namespace currency.mobile.Views {
                 return dt;
               }).ToArray();
             if (data.Length == 0) continue;
+
+            var grid = new Grid {
+              Padding = 7,
+              RowDefinitions = new RowDefinitionCollection(),
+              ColumnDefinitions = new ColumnDefinitionCollection {
+                new ColumnDefinition {Width = new GridLength(3, GridUnitType.Star)},
+                new ColumnDefinition {Width = new GridLength(2, GridUnitType.Star)},
+                new ColumnDefinition {Width = new GridLength(2, GridUnitType.Star)},
+                new ColumnDefinition {Width = new GridLength(2, GridUnitType.Star)},
+                new ColumnDefinition {Width = new GridLength(2, GridUnitType.Star)},
+              },
+            };
+      
+            grid.Children.Add(Utils.GetLabel(currencyCode.ToUpper(), 16, Color.LightCyan), 2, grid.RowDefinitions.Count);
+            grid.RowDefinitions.Add(new RowDefinition {Height = GridLength.Auto});
+
+            grid.Children.Add(Utils.GetLabel("Date", 14, Color.LightCyan), 0, grid.RowDefinitions.Count);
+            grid.Children.Add(Utils.GetLabel("Buy", 14, Color.LightCyan), 1, grid.RowDefinitions.Count);
+            grid.Children.Add(Utils.GetLabel("B delta", 14, Color.LightCyan), 2, grid.RowDefinitions.Count);
+            grid.Children.Add(Utils.GetLabel("Sell", 14, Color.LightCyan), 3, grid.RowDefinitions.Count);
+            grid.Children.Add(Utils.GetLabel("S delta", 14, Color.LightCyan), 4, grid.RowDefinitions.Count);
+            grid.RowDefinitions.Add(new RowDefinition {Height = GridLength.Auto});
+
+            var seriesB = new LineSeries { Title = currencyCode, MarkerType = MarkerType.Circle, Color = OxyColors.LightSeaGreen };
+            var seriesS = new LineSeries { Title = currencyCode, MarkerType = MarkerType.Circle, Color = OxyColors.LightSalmon };
 
             foreach (var item in data) {
               if (item.DateParsed.Date == today)
@@ -80,7 +88,23 @@ namespace currency.mobile.Views {
               grid.Children.Add(Utils.GetLabel($"{Utils.GetDirection(item.Rate_S_Delta)} {item.Rate_S_Delta:n3}", 16,
                   Utils.GetDiffColor(item.Rate_S_Delta)), 4, grid.RowDefinitions.Count);
               grid.RowDefinitions.Add(new RowDefinition {Height = GridLength.Auto});
+              
+              seriesB.Points.Add(new DataPoint(item.DateParsed.ToOADate(), (double)item.Rate_B));
+              seriesS.Points.Add(new DataPoint(item.DateParsed.ToOADate(), (double)item.Rate_S));
             }
+            
+            mainContent.Children.Add(grid);
+            mainContent.Children.Add(new PlotView {
+              VerticalOptions = LayoutOptions.FillAndExpand, HorizontalOptions = LayoutOptions.CenterAndExpand,
+              MinimumHeightRequest = 200, HeightRequest = 200,
+              IsEnabled = false,
+              Model = new PlotModel {
+                // Title = currencyCode, TitleColor = OxyColor.FromRgb(255, 255, 255),
+                TextColor = OxyColor.FromRgb(255, 255, 255), IsLegendVisible = false,
+                Axes = { new DateTimeAxis { StringFormat = "dd-MM", IsAxisVisible = true }, new LinearAxis { MinorStep = 0.01, MajorStep = 0.1} },
+                Series = { seriesB, seriesS },
+              }
+            });
           }
         }
         finally {
@@ -92,7 +116,7 @@ namespace currency.mobile.Views {
 
       refreshView.Content = new ScrollView {
         BackgroundColor = Color.Black,
-        Content = grid
+        Content = mainContent
       };
       Privat24Page.Content = refreshView;
 
