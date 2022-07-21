262e10e7e013b0ae847662918ab556bf924ec2f6
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2022-07-21 11:20:24 +0300

    allow only 1 instance of app to be running

--- currency.watcher/Program.cs
+++ currency.watcher/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace currency.watcher {
 
   static class Program {
+
+    static Mutex mutex = new Mutex(true, "{df1ad336-292a-4375-8fc6-109088e260df}");
 
     /// <summary>
     /// The main entry point for the application.
@@ -11,9 +14,16 @@ namespace currency.watcher {
     [STAThread]
     static void Main() {
 
+      if (!mutex.WaitOne(TimeSpan.Zero, true)) {
+        MessageBox.Show("Another instance of the application is already running.", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        Common.PostMessage((IntPtr)Common.HWND_BROADCAST, Common.WM_SHOWME, IntPtr.Zero, IntPtr.Zero);
+        Environment.Exit(0);
+      }
+
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new MainForm());
+      mutex.ReleaseMutex();
     }
   }
 }
