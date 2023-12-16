e0bd834a15d2fa79c1f4c46dfbe15a1722ab0ac5
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2023-12-16 23:29:52 +0200

    store settings in registry (portable mode)

--- currency.watcher/AppSettings.cs
+++ currency.watcher/AppSettings.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace currency.watcher {
 
@@ -10,45 +11,24 @@ namespace currency.watcher {
     private static readonly Lazy<AppSettings> InstanceField = new Lazy<AppSettings>(Load);
     internal static AppSettings Instance => InstanceField.Value;
 
-    public int Left;
-    public int Top;
-    public int Height;
-    public int Width;
-    public double Opacity;
-    public bool StickToEdges;
+    public int Left = Screen.PrimaryScreen.WorkingArea.Left; // Screen.PrimaryScreen.WorkingArea.Right - Width;
+    public int Top = Screen.PrimaryScreen.WorkingArea.Top;
+    public int Height = 245;
+    public int Width = 175;
+    public double Opacity = 1;
+    public bool StickToEdges = true;
     
     public int ChartViewMode;
     public bool ChartLines;
     
     public int CurrencyIndex;
-    public readonly int RefreshInterval;
+    public readonly int RefreshInterval = 30;
 
-    public int MainPanWidth;
-    public int[] FinanceHistorySizes;
-    public int[] HistorySizes;
-    public int NbuHistoryWidth;
-    public int[] NbuHistorySizes;
-
-    public AppSettings() {
-      Left = Screen.PrimaryScreen.WorkingArea.Left;// Screen.PrimaryScreen.WorkingArea.Right - Width;
-      Top = Screen.PrimaryScreen.WorkingArea.Top;
-      Height = 245;
-      Width = 175;
-      Opacity = 1;
-      StickToEdges = true;
-    
-      ChartViewMode = 0;
-      ChartLines = false;
-
-      CurrencyIndex = 0;
-      RefreshInterval = 30;
-
-      MainPanWidth = 159;
-      FinanceHistorySizes = new int[3] { 40, 50, 50 };
-      HistorySizes = new int[3] { 85, 55, 55 };
-      NbuHistoryWidth = 200;
-      NbuHistorySizes = new int[2] { 100, 90 };
-    }
+    public int MainPanWidth = 159;
+    public int[] FinanceHistorySizes = { 40, 50, 50 };
+    public int[] HistorySizes = { 85, 55, 55 };
+    public int NbuHistoryWidth = 200;
+    public int[] NbuHistorySizes = { 100, 90 };
 
     #region Load/Save
 
@@ -61,12 +41,77 @@ namespace currency.watcher {
     }
 
     internal void Save() {
+      var key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\sergiye\\currency.watcher");
+      if (key == null) {
+        SaveToFile();
+        return;
+      }
+      key.SetValue("Height", Height);
+      key.SetValue("Width", Width);
+      key.SetValue("Top", Top);
+      key.SetValue("Left", Left);
+      key.SetValue("Opacity", Opacity);
+      key.SetValue("StickToEdges", StickToEdges);
+        
+      // key.SetValue("RefreshInterval")?.ToString(), out result.RefreshInterval);
+
+      key.SetValue("CurrencyIndex", CurrencyIndex);
+      key.SetValue("ChartViewMode", ChartViewMode);
+      key.SetValue("ChartLines", ChartLines);
+      key.SetValue("MainPanWidth", MainPanWidth);
+      key.SetValue("FinanceHistorySizes1", FinanceHistorySizes[0]);
+      key.SetValue("FinanceHistorySizes2", FinanceHistorySizes[1]);
+      key.SetValue("FinanceHistorySizes3", FinanceHistorySizes[2]);
+      key.SetValue("HistorySizes1", HistorySizes[0]);
+      key.SetValue("HistorySizes2", HistorySizes[1]);
+      key.SetValue("HistorySizes3", HistorySizes[2]);
+      key.SetValue("NbuHistorySizes1", NbuHistorySizes[0]);
+      key.SetValue("NbuHistorySizes2", NbuHistorySizes[1]);
+
+      key.SetValue("NbuHistoryWidth", NbuHistoryWidth);
+    }
+
+    private static AppSettings Load() {
+      var key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\sergiye\\currency.watcher");
+      var result = new AppSettings();
+      if (key == null) {
+        LoadFromFile();
+        return result;
+      }
+      int.TryParse(key.GetValue("Height")?.ToString(), out result.Height);
+      int.TryParse(key.GetValue("Width")?.ToString(), out result.Width);
+      int.TryParse(key.GetValue("Top")?.ToString(), out result.Top);
+      int.TryParse(key.GetValue("Left")?.ToString(), out result.Left);
+      double.TryParse(key.GetValue("Opacity")?.ToString(), out result.Opacity);
+      bool.TryParse(key.GetValue("StickToEdges")?.ToString(), out result.StickToEdges);
+        
+      // int.TryParse(key.GetValue("RefreshInterval")?.ToString(), out result.RefreshInterval);
+
+      int.TryParse(key.GetValue("CurrencyIndex")?.ToString(), out result.CurrencyIndex);
+      int.TryParse(key.GetValue("ChartViewMode")?.ToString(), out result.ChartViewMode);
+      bool.TryParse(key.GetValue("ChartLines")?.ToString(), out result.ChartLines);
+      int.TryParse(key.GetValue("MainPanWidth")?.ToString(), out result.MainPanWidth);
+      int.TryParse(key.GetValue("FinanceHistorySizes1")?.ToString(), out result.FinanceHistorySizes[0]);
+      int.TryParse(key.GetValue("FinanceHistorySizes2")?.ToString(), out result.FinanceHistorySizes[1]);
+      int.TryParse(key.GetValue("FinanceHistorySizes3")?.ToString(), out result.FinanceHistorySizes[2]);
+      int.TryParse(key.GetValue("HistorySizes1")?.ToString(), out result.HistorySizes[0]);
+      int.TryParse(key.GetValue("HistorySizes2")?.ToString(), out result.HistorySizes[1]);
+      int.TryParse(key.GetValue("HistorySizes3")?.ToString(), out result.HistorySizes[2]);
+      int.TryParse(key.GetValue("NbuHistorySizes1")?.ToString(), out result.NbuHistorySizes[0]);
+      int.TryParse(key.GetValue("NbuHistorySizes2")?.ToString(), out result.NbuHistorySizes[1]);
+        
+      int.TryParse(key.GetValue("NbuHistoryWidth")?.ToString(), out result.NbuHistoryWidth);
+
+      return result;
+    }
+
+    internal void SaveToFile() {
       var fileName = GetSettingsFilePath();
       GC.Collect(GC.MaxGeneration);
       File.WriteAllText(fileName, this.ToJson());
     }
 
-    private static AppSettings Load() {
+    private static AppSettings LoadFromFile() {
       var fileName = GetSettingsFilePath();
       try {
         if (File.Exists(fileName)) {
