cb3e3d34a63a34bbc8b24c1199757e4936167009
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2024-08-24 18:29:23 +0300

    fix clean script

--- clean.bat
+++ clean.bat
@@ -9,3 +9,4 @@ rmdir /s /q .\currency.watcher\obj
 del /S ".\currency.watcher\bin\*.dll"
 del /S ".\currency.watcher\bin\*.pdb"
 del /S ".\currency.watcher\bin\*.xml"
+del /S ".\currency.watcher\FodyWeavers.xsd"
